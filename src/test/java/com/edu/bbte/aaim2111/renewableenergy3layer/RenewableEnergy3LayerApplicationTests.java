e48764a1eb3bbdd1679a4bb8204ecc475063baa8
ArintonAkos <arintonakos@gmail.com>
2024-02-06 22:38:09 +0200

    Fixed build error.

--- src/test/java/com/edu/bbte/aaim2111/renewableenergy3layer/RenewableEnergy3LayerApplicationTests.java
+++ src/test/java/com/edu/bbte/aaim2111/renewableenergy3layer/RenewableEnergy3LayerApplicationTests.java
@@ -2,8 +2,10 @@ package com.edu.bbte.aaim2111.renewableenergy3layer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RenewableEnergy3LayerApplicationTests {
 
 	@Test
